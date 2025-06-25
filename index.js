235e8f702f76080ba53b69ab68572c263403ebf7
thaddydore <thaddydore@gmail.com>
2025-06-25 08:30:59 +0100

    health check endpoint

--- index.js
+++ index.js
@@ -15,5 +15,6 @@ app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
 app.use('/api/v1', nftRoute);
+app.use('/api/v1/healthz', (_, res) => res.send('OK'));
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
