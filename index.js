e841d40cda24e50583f7b64710257d55e5aaa1da
thaddydore <thaddydore@gmail.com>
2025-06-25 09:04:46 +0100

    enable cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 const { createImageFolderForFileStorage } = require('./util/createImageFolderForStorage');
 const { nftRoute } = require('./routes/nft');
 
@@ -10,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 createImageFolderForFileStorage();
 
 const app = express();
-
+app.use(cors);
 app.use(express.json({ limit: '5mb' }));
 app.use(express.urlencoded({ extended: true, limit: '5mb' }));
 
